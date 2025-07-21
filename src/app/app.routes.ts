a97436b2f32a2ab963da58df2cdc5ee31144a938
Tyler Fenton <tsfento@gmail.com>
2025-07-21 09:19:15 -0500

    Added guards to Fullstack 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
+import { noAuthGuard } from './no-auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadComponent: () => import('./features/home/home.component').then((c) => c.HomeComponent),
+    canActivate: [authGuard],
   },
   {
     path: 'login',
     loadComponent: () => import('./features/auth/login/login.component').then((c) => c.LoginComponent),
+    canActivate: [noAuthGuard],
   },
 ];
