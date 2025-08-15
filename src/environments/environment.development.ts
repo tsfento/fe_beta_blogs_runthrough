590eefd806749a6cd25ecabba8761532f4ea7d00
Tyler Fenton <tsfento@gmail.com>
2025-08-15 13:34:51 -0500

    Adding environments files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
+  pusher: {
+    key: '5510b6f90e8cf800a69e',
+    cluster: 'us2'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://beta-blogs-runthrough.onrender.com',
+  pusher: {
+    key: '5510b6f90e8cf800a69e',
+    cluster: 'us2'
+  }
 };
